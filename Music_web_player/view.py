8bc3a4e159b013d203a1ad5485f3b849549bc035
Colinjog <18728620849@163.com>
2018-11-05 23:58:55 +0800

    不清楚，传入的music_list没有在模板中渲染

--- Music_web_player/view.py
+++ Music_web_player/view.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+from Music_web_player.API.NetEase.api import NetEase, Parse
 def index(request):
-    if request.COOKIES.get('username'):
-        print(request.COOKIES)
-        context = {'music_list': [
-            {'music_title': "海阔天空", 'pic_src': 'http://p1.music.126.net/QHw-RuMwfQkmgtiyRpGs0Q==/102254581395219.jpg',
-             'music_src': 'http://m10.music.126.net/20181104232354/f95ec5e6b913376846c8db9c3cf48e60/ymusic/c588/bb36/c035/3cf45c286508297bec4135084556cf05.mp3',
-             "time": 200, 'author': "Beyond"}, ]}
-        return render(request, 'MainView/index.html',context=context)
-    else:
-        return HttpResponseRedirect(reverse('login:sign_in'))
+    # if request.COOKIES.get('username'):
+    print(request.COOKIES)
+    context = {}
+
+    context['music_list '] = _search('shake it off')
+
+    return render(request, 'MainView/index.html',context=context)
+    # else:
+    #     return HttpResponseRedirect(reverse('login:sign_in'))
 
 
 def default_index(request):
@@ -26,3 +26,47 @@ def search(request):
     if request.method == "POST":
         pass
 
+
+def _search(music):
+    api = NetEase()
+    songs = api.search(music)
+
+    '''
+        music list
+        music_title music_pic_src music_src author time album_pic_src music_id
+    '''
+    songs = songs['songs']
+    music_list = []
+    try:
+
+        for song in songs:
+            temp = {}
+            temp['music_id'] = song['id']
+            temp['music_title'] = song['name']
+            temp['author'] = song['artists'][0]['name']
+            songs_url = api.songs_url(ids=[temp['music_id']])
+            album_id = song['album']['id']
+            if songs_url:
+                temp['music_src'] = songs_url[0]['url']
+            else:
+                temp['music_src'] = ""
+
+            temp['album_pic_src'] = api.album(album_id)[0]['al']['picUrl']
+            if not 'picUrl' in song.keys():
+                temp['music_pic_src'] = temp['album_pic_src']
+            else:
+                temp['music_pic_src'] = song['picUrl']
+            temp['tlyrics'] = api.song_tlyric(temp['music_id'])
+            # print(temp)
+            music_list.append(temp)
+            if len(music_list)>=10:
+                break
+    except KeyError:
+        print("KeyError")
+    finally:
+        pass
+    return music_list
+
+if __name__=="__main__":
+    _search("hello")
+    _search('shake it off')
