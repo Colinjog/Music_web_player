e89e1da4a608209efada47e5e15862b18bbfd331
Colinjog <18728620849@163.com>
2018-11-08 23:51:43 +0800

    修复了播放的bug

--- static/js/new/Player.js
+++ static/js/new/Player.js
@@ -19,3 +19,29 @@ function aplayerLoadingSong(musicList,type){
         });
     }
 }
+var cur_playing_music_id = "0"
+
+function stopPlay(){
+    music = document.getElementById(cur_playing_music_id);
+    if (music){
+        music.pause();
+    }
+}
+function playThis(id){
+    if (id==cur_playing_music_id){
+        ;
+    }
+    else{
+        stopPlay();
+        music = document.getElementById(id);
+        if (music){
+            music.load()
+            cur_playing_music_id = id;
+            console.log("cur_playing_music_id:"+cur_playing_music_id)
+            music.play();
+        }
+        else{
+            console.log(id,"音乐不存在");
+        }
+    }
+}
