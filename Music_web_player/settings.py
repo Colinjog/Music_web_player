a8e3f9c633aa9ba8e1c9da5d8083c0243e77dee7
Colinjog <18728620849@163.com>
2018-11-18 17:45:30 +0800

    bug

--- Music_web_player/settings.py
+++ Music_web_player/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '##!9!5-^2a)-+9tdmn^38_h!zbbnm2^#c@wetn0nv-x+-%k+d6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
